986e87e48444e33b6ef9ee09b9f92e98a45ce5ab
Henry Lee <henryleeworld@gmail.com>
2023-05-04 00:12:15 +0800

    feat:升級至 laravel 9

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('users/show', [UsersController::class, 'show']);
 Route::get('users/pdf', [UsersController::class, 'createPDF']);
